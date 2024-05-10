02e2bc88c721ce5e8eb24aaa0892bbed0b62448c
Subramanya Chakravarthy <chanty.chakravarthy@gmail.com>
2024-05-10 23:52:03 +0530

    working app

--- src/Components/CategoriesList.tsx
+++ src/Components/CategoriesList.tsx
@@ -1,30 +1,31 @@
-import React from 'react'
+import React from "react";
 import { Category } from "./Category";
-import { observer, useMount } from '@legendapp/state/react';
-import type { CategoriesListObservable } from '../LegendState/Category.model';
+import { observer, useMount } from "@legendapp/state/react";
+import type { CategoriesListObservable } from "../LegendState/Category.model";
 
 interface CategoriesListProps {
-  categories: CategoriesListObservable
+  categories: CategoriesListObservable;
   onCategoryPressed: (id: string) => void;
   onCategoryDelete: (id: string) => void;
 }
 
-export const CategoriesList = observer(({
-  categories,
-  onCategoryPressed,
-  onCategoryDelete,
-}: CategoriesListProps) => {
+export const CategoriesList = observer(
+  ({
+    categories,
+    onCategoryPressed,
+    onCategoryDelete,
+  }: CategoriesListProps) => {
+    console.log({ categories: categories?.peek?.() });
 
-  console.log({categories: categories.peek()})
-
-  return categories.map((category) => {
-    return (
-      <Category
-        key={category.id.peek()}
-        category={category}
-        onCategoryPressed={onCategoryPressed}
-        onCategoryDelete={onCategoryDelete}
-      />
-    );
-  });
-});
+    return categories?.map((category) => {
+      return (
+        <Category
+          key={category.id.peek()}
+          category={category}
+          onCategoryPressed={onCategoryPressed}
+          onCategoryDelete={onCategoryDelete}
+        />
+      );
+    });
+  }
+);
